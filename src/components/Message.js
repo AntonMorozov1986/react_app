a3e34fc2f832c979e09c6a7f62a8ecfac01a5274
Anton Morozov <amorozov86@gmail.com>
2021-10-06 01:38:17 +0300

    Rollback to start

--- src/components/Message.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export function Message({ user }) {
-    return <h3>{ user.name }, Добро пожаловать в ChatiX</h3>;
-}
-Message.propTypes = {
-    user: PropTypes.object,
-};
--- src/global/constants.js
+++
@@ -1,6 +0,0 @@
-export const USER = {
-    name: 'Александр',
-};
-export const APP = {
-    name: APP_NAME,
-};
